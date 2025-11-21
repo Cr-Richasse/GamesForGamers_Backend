6a057e8b16988a466d3dc4c02295974faa0dbb6e
Cr-Richasse <cr.richasse@duocuc.cl>
2025-11-20 21:07:40 -0300

    primer commit

---
+++ src/main/java/com/example/GamesForGamers/GamesForGamersApplication.java
@@ -0,0 +1,13 @@
+package com.example.GamesForGamers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesForGamersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesForGamersApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/example/GamesForGamers/GamesForGamersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GamesForGamers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesForGamersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
